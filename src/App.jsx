cabe2b76b495ea850c8e38d82ddaac7c1eb6c0e5
Gabriel <lourencogabriel77@gmail.com>
2025-06-06 21:54:28 -0300

    feat: Add new assets and implement Hero section in the app
    - Added various image assets including logos for Node, Python, React, and others.
    - Introduced SVG icons for menu and close actions.
    - Added 3D models for computer, Git, Node, and Python.
    - Created a new Hero section in App.jsx to enhance the landing page.
    - Implemented dynamic text display with images in the Hero section using words from constants.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
@@ -5,7 +6,8 @@ const App = () => {
       <div>
         <h1 className="text-3xl underline">Welcome to my 3D Portifolio!</h1>
       </div>
+      <Hero />
     </main>
   );
-}
+};
 export default App;
