0c38cea8aead1266c089cd1bdb255f6b9c890e89
Gabriel <lourencogabriel77@gmail.com>
2025-06-08 16:19:27 -0300

    feat(hero): improve animation, lighting, and add snow effect
    - Enhanced the HeroExperience animation and lighting for a more immersive look
    - Improved light positioning and intensity
    - Added a snow particle effect to the Hero section
    - Added a transition effect to the h1 in the Hero section
    - Removed border styling used for component debugging

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,27 @@
 import { words } from "../constants";
 import Button from "../components/Button";
 import HeroExperience from "../components/HeroModels/HeroExperience";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      {
+        y: 50,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.2,
+        duration: 1,
+        ease: "power2.inOut",
+      }
+    );
+  });
+
   return (
     <section id="hero" className="relative overflow-hidden">
       <div className="absolute top-0 left-0 z-10">
@@ -13,7 +32,7 @@ const Hero = () => {
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
           {/* right: 3D */}
           <figure>
-            <div className="hero-3d-layout border-red-200 border-2">
+            <div className="hero-3d-layout">
               <HeroExperience />
             </div>
           </figure>
@@ -48,7 +67,7 @@ const Hero = () => {
             </p>
             <Button
               id="button"
-              className="md:w-80 md:h-16 w-6 h-12"
+              className="md:w-80 md:h-16 w-60 h-12"
               text="See my Work"
             />
           </div>
