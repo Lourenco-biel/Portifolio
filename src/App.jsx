c3de96f88ee6424af0a141bd1ba454beb13cc3e6
Gabriel <lourencogabriel77@gmail.com>
2025-06-08 21:07:28 -0300

    feat(FeatureCards): add FeatureCards component to display abilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import NavBar from './components/NavBar';
+import FeatureCards from './sections/FeatureCards';
 import Hero from './sections/Hero';
 import LogoShowcase from './sections/LogoShowcase';
 import ShowcaseSection from './sections/ShowcaseSection';
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <ShowcaseSection />
       <LogoShowcase />
+      <FeatureCards />
     </>
   );
 };
