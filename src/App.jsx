5c80c457748129c32a0cb8e2619baa070570e588
Gabriel <lourencogabriel77@gmail.com>
2025-06-09 00:01:08 -0300

    feat(ExperienceSection): add ExperienceSection with GSAP animations, GlowCard, and TitleHeader components
    Created the ExperienceSection component to showcase my professional work experiences with animated transitions using GSAP.
    
    - add the GlowCard component.
    - add the TitleHeader component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import NavBar from './components/NavBar';
+import ExperienceSection from './sections/ExperienceSection';
 import FeatureCards from './sections/FeatureCards';
 import Hero from './sections/Hero';
 import LogoShowcase from './sections/LogoShowcase';
@@ -12,6 +13,7 @@ const App = () => {
       <ShowcaseSection />
       <LogoShowcase />
       <FeatureCards />
+      <ExperienceSection />
     </>
   );
 };
