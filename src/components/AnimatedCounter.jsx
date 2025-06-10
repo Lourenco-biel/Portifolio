9aefe60a2b4b55c360705d444ccda4c7fe63e33e
Gabriel <lourencogabriel77@gmail.com>
2025-06-10 13:44:47 -0300

    feat(AnimatedCounter): integrate react-intersection-observer for animated counting on scroll

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -1,21 +1,30 @@
-import CountUp from "react-countup";
-import { counterItems } from "../constants";
+import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
+import { counterItems } from '../constants';
 
 const AnimatedCounter = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  });
+
   return (
     <div id="counter" className="padding-x-lg xl:mt-0 mt-32">
       <div className="mx-auto grid-4-cols">
-        {counterItems.map((item) => (
-          <div className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
+        {counterItems.map((item) => {
+          return (
             <div
-              key={counterItems.label}
-              className="counter-number text-white text-5xl font-bold mb-2"
+              ref={ref}
+              key={item.label}
+              className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center"
             >
-              <CountUp suffix={item.suffix} end={item.value} />
+              <div className="counter-number text-white text-5xl font-bold mb-2">
+                {inView && <CountUp suffix={item.suffix} end={item.value} />}
+              </div>
+              <div className="text-white-50 text-lg">{item.label}</div>
             </div>
-            <div className="text-white-50 text-lg">{item.label}</div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
