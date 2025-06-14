798e40e2f2088f6a313d468b317ff2ae15194bf1
Gabriel <lourencogabriel77@gmail.com>
2025-06-14 19:35:39 -0300

    feat(FeatureCards): adjust image size and styling for feature cards

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -9,8 +9,8 @@ const FeatureCards = () => {
             key={title}
             className="card-border rounded-xl p-8 flex flex-col gap-4"
           >
-            <div className="size-14 flex items-center justify-center rounded-full">
-              <img src={imgPath} alt={title} />
+            <div className="size-10 flex items-center justify-center rounded-full">
+              <img src={imgPath} alt={title} className="w-8 h-8 object-cover" />
             </div>
             <h3 className="text-white text-2xl font-semibold mt-2">{title}</h3>
             <p className="text-white-50 text-lg">{desc}</p>
