8b2970998e22d9ba97cb62260664059361a91e01
Gabriel <lourencogabriel77@gmail.com>
2025-06-20 21:51:54 -0300

    fix(footer): change social icons from div to anchor elements for better accessibility

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => {
         </div>
         <div className="socials">
           {socialImgs.map((socialImg, index) => (
-            <div key={index} className="icon">
+            <a key={index} className="icon" href={socialImg.url}>
               <img src={socialImg.imgPath} alt="social icon" />
-            </div>
+            </a>
           ))}
         </div>
         <div className="flex flex-col justify-center">
