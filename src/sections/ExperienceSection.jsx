bc7b6b8799fd1c2e95a57b16393638c35bdb1cd9
Gabriel <lourencogabriel77@gmail.com>
2025-06-20 21:52:41 -0300

    fix(experience-section): update exp-card key to include date and index for uniqueness

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -106,8 +106,11 @@ const ExperienceSection = () => {
         />
         <div className="mt-32 relative">
           <div className="relative z-50 xl:space-y-32 space-y-10">
-            {expCards.map((card) => (
-              <div key={card.title} className="exp-card-wrapper">
+            {expCards.map((card, idx) => (
+              <div
+                key={card.title + card.date + idx}
+                className="exp-card-wrapper"
+              >
                 <div className="xl:w-2/6">
                   <GlowCard card={card}>
                     <div className="w-60 h-24 flex items-center justify-normal">
